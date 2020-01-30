8fb3c580e315f0a6fe7933a27aef86f776872d57
Kevin M Granger <kgranger@redhat.com>
2020-01-30 15:39:55 -0600

    Simplify registration code checking

--- src/registrationCodes.js
+++ src/registrationCodes.js
@@ -1,86 +1,82 @@
 const { ApplicationError } = require('./errorHandling');
 
 /**
- * Makes the special code checking closure.
+ * Check if the given code is a valid special code.
  * "Special Codes" are tracked in firebase instead of salesforce.
  * @param firebase
- * @returns {(code: string): object|undefined}
+ * @param {string} code
  */
-function makeSpecialCodeChecker(firebase) {
-  return async function checkSpecialCode(code) {
-    const registrationCodes = await firebase.getReference('registration-codes');
-    switch (code) {
-      case registractionCodes['individual-volunteer-distributors']:
-        return {
-          success: true,
-          volunteerType: 'volunteerDistributor',
-          isIndividualDistributor: true
-        };
-      case registrationCodes['tat-ambassadors']:
-        return {
-          success: true,
-          volunteerType: 'ambassadorVolunteer'
-        };
-    }
-  };
+async function checkSpecialCode(firebase, code) {
+  const registrationCodes = await firebase.getReference('registration-codes');
+  switch (code) {
+    case registractionCodes['individual-volunteer-distributors']:
+      return {
+        success: true,
+        volunteerType: 'volunteerDistributor',
+        isIndividualDistributor: true
+      };
+    case registrationCodes['tat-ambassadors']:
+      return {
+        success: true,
+        volunteerType: 'ambassadorVolunteer'
+      };
+  }
 }
 
 /**
- * Makes the regular code checking closure.
+ * Check if the given code is a valid regular code.
  * Regular codes are tracked in salesforce.
- * @param salesforce
- * @returns {(code: string): object}
+ * @param {*} salesforce
+ * @param {string} code
  */
-function makeRegularCodeChecker(salesforce) {
-  return async function checkRegularCode(code) {
-    // TODO: get from salesforce
-    // Unsure what the interface would look like for this query.
-    const records = [];
+async function checkRegularCode(salesforce, code) {
+  // TODO: get from salesforce
+  // Unsure what the interface would look like for this query.
+  const records = [];
 
-    if (records.length === 0) {
-      throw new ApplicationError({
-        message: 'The registration code was incorrect.',
-        errorCode: 'INCORRECT_REGISTRATION_CODE'
-      });
-    }
+  if (records.length === 0) {
+    throw new ApplicationError({
+      message: 'The registration code was incorrect.',
+      errorCode: 'INCORRECT_REGISTRATION_CODE'
+    });
+  }
 
-    // TODO: we should eventually check that there was only 1 record,
-    // but the original code did not.
-    const accountId = records[0].Id;
+  // TODO: we should eventually check that there was only 1 record,
+  // but the original code did not.
+  const accountId = records[0].Id;
 
-    // TODO: get from salesforce
-    // Unsure what the interface would look like for this query.
-    // The result is an object with FirstName, LastName, and Id.
-    // The original code converts it to a nicer object with
-    // name, salesforceId
-    const coordinators = [];
+  // TODO: get from salesforce
+  // Unsure what the interface would look like for this query.
+  // The result is an object with FirstName, LastName, and Id.
+  // The original code converts it to a nicer object with
+  // name, salesforceId
+  const coordinators = [];
 
-    return {
-      success: true,
-      accountId: accountId,
-      volunteerType: 'volunteerDistributor',
-      isIndividualDistributor: false,
-      teamCoordinators: coordinators
-    };
+  return {
+    success: true,
+    accountId: accountId,
+    volunteerType: 'volunteerDistributor',
+    isIndividualDistributor: false,
+    teamCoordinators: coordinators
   };
 }
 
-/**
- * Composes the two checkers once they have been created.
- * Is perhaps evidence that this should just be a class.
- * @param specialCodeChecker
- * @param regularCodeChecker
- */
-function makeCombinedCodeChecker(specialCodeChecker, regularCodeChecker) {
-  return async function checkRegistrationCode(code) {
-    const specialCodeResult = await specialCodeChecker(code);
-    if (specialCodeResult !== undefined) return specialCodeResult;
-    return await regularCodeChecker(code);
-  };
+class CombinedCodeChecker {
+  constructor(firebase, salesforce) {
+    this.specialCodeChecker = checkSpecialCode.bind(null, firebase);
+    this.regularCodeChecker = checkRegularCode.bind(null, salesforce);
+  }
+
+  async check(code) {
+    return (
+      (await this.specialCodeChecker(code)) ||
+      (await this.regularCodeChecker(code))
+    );
+  }
 }
 
 module.exports = {
-  makeSpecialCodeChecker,
-  makeRegularCodeChecker,
-  makeCombinedCodeChecker
+  checkSpecialCode,
+  checkRegularCode,
+  CombinedCodeChecker
 };
